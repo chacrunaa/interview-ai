693a3eb792a1e51fa32cdcbd3b89f707e5a8c54c
unknown <andreyandreydn16@gmail.com>
2024-05-13 23:23:30 +0300

    update main.py file

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# import os
+# # import os
 # import whisper
-# import warnings
-# import logging
-# import time
+# # import warnings
+# # import logging
+# # import time
 
-# logging.basicConfig(filename='/app/log.log', level=logging.INFO, format='%(asctime)s - %(message)s')
-# logger = logging.getLogger()
+# # logging.basicConfig(filename='/app/log.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+# # logger = logging.getLogger()
 
-# warnings.simplefilter("ignore")
+# # warnings.simplefilter("ignore")
 
 # # Загрузка модели Whisper
-# video_files = [f for f in os.listdir("/app/videos") if os.path.isfile(os.path.join("/app/videos", f))]
+# # video_files = [f for f in os.listdir("/app/videos") if os.path.isfile(os.path.join("/app/videos", f))]
 
 
 # model = whisper.load_model("base")  # Можно выбрать другую модель, например 'small', 'medium', 'large', или 'tiny'
@@ -37,12 +37,16 @@
 
 # logger.info(f"API requests: {api_requests}")
 # logger.info(f"Execution time: {execution_time} seconds")
+# # import whisper
+# # import warnings
+# # warnings.simplefilter("ignore")
+
+# # model = whisper.load_model("base")
+
+# # result = model.transcribe(audio="/videos/IBS.mp4")
+
+# # print(result["text"]
 import whisper
-import warnings
-warnings.simplefilter("ignore")
-
 model = whisper.load_model("base")
-
-result = model.transcribe(audio="/videos/IBS.mp4")
-
-print(result["text"])
+result = model.transcribe("/videos/")
+print(f' The text in video: \n {result["text"]}'))
